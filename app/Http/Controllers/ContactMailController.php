44825f6116f8e0fc91ba639b784c9cde3aec5756
ryuma-eng <rjdvd0615@gmail.com>
2026-07-23 01:06:55 +0800

    try brevo mail

--- app/Http/Controllers/ContactMailController.php
+++ app/Http/Controllers/ContactMailController.php
@@ -24,72 +24,73 @@ class ContactMailController extends Controller
         
         try {
       
-            // Mail::to($developer)->send(new ContactMessage($fields));
+            Mail::to($developer)->send(new ContactMessage($fields));
 
-            // Mail::to($fields['email'])->send(new ContactMail($fields));
+            Mail::to($fields['email'])->send(new ContactMail($fields));
 
-            Resend::emails()->send([
-                'from' => env('MAIL_FROM_ADDRESS'),
-                'to' => [
-                    $fields['email']
-                ],
-                'subject' => 'Message Received Successfully',
-                'html' => "
-                    <p>
-                        Dear {$fields['name']}
-                    </p>
+            // Resend::emails()->send([
+            //     // 'from' => env('MAIL_FROM_ADDRESS'),
+            //     'from' => "env('MAIL_FROM_ADDRESS')",
+            //     'to' => [
+            //         $fields['email']
+            //     ],
+            //     'subject' => 'Message Received Successfully',
+            //     'html' => "
+            //         <p>
+            //             Dear {$fields['name']}
+            //         </p>
 
-                    <p>
-                        Thank you for reaching out!
-                    </p>
+            //         <p>
+            //             Thank you for reaching out!
+            //         </p>
 
-                    <p>
-                        This email is to confirm that I have successfully received your message.
-                        I'll review it and get back to you as soon as I can.
-                    </p>
+            //         <p>
+            //             This email is to confirm that I have successfully received your message.
+            //             I'll review it and get back to you as soon as I can.
+            //         </p>
 
-                    <p>
-                        Best regards,
-                    </p>
+            //         <p>
+            //             Best regards,
+            //         </p>
 
-                    <p>
-                        Ricardo Jose David
-                    </p>
+            //         <p>
+            //             Ricardo Jose David
+            //         </p>
 
-                    <p>
-                        " . env('MAIL_FROM_ADDRESS') . "
-                    </p>
-                "
-            ]);
+            //         <p>
+            //             " . env('MAIL_FROM_ADDRESS') . "
+            //         </p>
+            //     "
+            // ]);
 
-            Resend::emails()->send([
-                'from' => env('MAIL_FROM_ADDRESS'),
-                'to' => [
-                    env('MAIL_FROM_ADDRESS')
-                ],
-                'subject' => 'New Contact Message from ' . $fields['name'],
-                'html' => "
-                    <p>
-                        Dear Mr. David
-                    </p>
+            // Resend::emails()->send([
+            //     'from' => env('MAIL_FROM_ADDRESS'),
+            //     'to' => [
+            //         env('MAIL_FROM_ADDRESS')
+            //     ],
+            //     'subject' => 'New Contact Message from ' . $fields['name'],
+            //     'html' => "
+            //         <p>
+            //             Dear Mr. David
+            //         </p>
 
-                    <p>
-                        {$fields['message']}
-                    </p>
+            //         <p>
+            //             {$fields['message']}
+            //         </p>
 
-                    <p>
-                        Best Regards,
-                    </p>
+            //         <p>
+            //             Best Regards,
+            //         </p>
 
-                    <p>
-                        {$fields['name']}
-                    </p>
+            //         <p>
+            //             {$fields['name']}
+            //         </p>
 
-                    <p>
-                        {$fields['email']}
-                    </p>
-                "
-            ]);
+            //         <p>
+            //             {$fields['email']}
+            //         </p>
+            //     "
+            // ]);
 
             return response()->json([
                 'message' => 'Emails sent successfully.'
